d66b88763cd22b771d7ba2c5c520f62f80b82ed2
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2021-12-08 21:31:06 +0700

    Chapter 3: Add Styling Tips

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Layout from '../../src/components/Layout'
+
+import Alert from '../../src/components/Alert'
 
 export default function FirstPost() {
   return (
@@ -10,6 +11,7 @@ export default function FirstPost() {
       </Head>
 
       <h1>First Post</h1>
+      <Alert type="success">Nice!</Alert>
     </Layout>
   )
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import styles from './index.module.css'
+import styles from './layout.module.css'
 import utilStyles from '../../../styles/utils.module.css'
 
 const name = 'Nathakorn'
